334c3cf82e1b72a3320fec7570cdb1af705c1a43
AdrianBozecki <mail.bozecki@gmail.com>
2023-06-03 19:56:27 +0200

    podstawowe logowanie

---
+++ src/models/User.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace models;
+
+class User
+{
+    private $email;
+    private $password;
+    private $name;
+    private $lastname;
+
+
+    public function __construct(string $email,string $password,string $name,string $lastname)
+    {
+        $this->email = $email;
+        $this->password = $password;
+        $this->name = $name;
+        $this->lastname = $lastname;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+
+
+
+}
