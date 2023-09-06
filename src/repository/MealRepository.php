c9cc492001ddd4aee31154ceeb24e1d73b3ec961
AdrianBozecki <mail.bozecki@gmail.com>
2023-09-06 20:53:22 +0200

    added other controller

--- src/repository/MealRepository.php
+++ src/repository/MealRepository.php
@@ -85,31 +85,6 @@ class MealRepository extends Repository
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    public function addMeal(Meal $meal): void
-    {
-        session_start();
-
-        $date = new DateTime();
-        $stmt = $this->database->connect()->prepare(
-            'INSERT INTO public.meals (title, preparation, ingredients, created_at, id_user, image, category, "like", dislike)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)'
-        );
-
-        $idUser = $_SESSION['user_id'];
-        $imagePath = $meal->getImage() ? $meal->getImage() : 'public/uploads/default.png';
-
-        $stmt->execute([
-            $meal->getTitle(),
-            $meal->getPreparation(),
-            $meal->getIngredients(),
-            $date->format('Y-m-d'),
-            $idUser,
-            $imagePath,
-            $meal->getCategory(),
-            $meal->getLike(),
-            $meal->getDislike(),
-        ]);
-    }
 
     public function getMealByTitle(string $searchString)
     {
